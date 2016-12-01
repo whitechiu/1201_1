205e349ba4a0c71f50de6f205393b899613bd968
GitTest <v304245@yahoo.com.tw>
2016-12-01 16:35:47 +0800

    Initial commit

---
+++ app/src/main/java/test/project/a1201_1/MyService.java
@@ -0,0 +1,16 @@
+package test.project.a1201_1;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
