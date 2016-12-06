f6785440e8435258271c170f5c12a3e3be42e989
GitTest <v304245@yahoo.com.tw>
2016-12-06 16:01:49 +0800

    1

--- app/src/main/java/test/project/a1201_1/MyService.java
+++ app/src/main/java/test/project/a1201_1/MyService.java
@@ -3,14 +3,22 @@ package test.project.a1201_1;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
-public class MyService extends Service {
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyService extends FirebaseInstanceIdService {
     public MyService() {
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("FCM", "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        //sendRegistrationToServer(refreshedToken);
     }
 }
